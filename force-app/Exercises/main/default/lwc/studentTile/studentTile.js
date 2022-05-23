d114948610898edcd61c0fae754b87cbed545551
Jang Hyemin <hmjang@i2max.co.kr>
2022-05-23 15:59:44 +0900

    Exercise 2-5 Completed

--- force-app/Exercises/main/default/lwc/studentTile/studentTile.js
+++ force-app/Exercises/main/default/lwc/studentTile/studentTile.js
@@ -11,5 +11,9 @@ export default class StudentTile extends LightningElement {
         get tileSelected() {
             return this.isSelected ? "tile selected" : "tile";
             }
+
+    studentClick(){
+        alert(this.student.Name);
+    }
 }
 
