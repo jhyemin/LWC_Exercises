5c6199282aaa2e28b3b2c673be60e2413b2ec5b1
Jang Hyemin <hmjang@i2max.co.kr>
2022-05-23 15:22:10 +0900

    Exercise 2-4 Completed

---
+++ force-app/Exercises/main/default/lwc/studentTile/studentTile.js
@@ -0,0 +1,15 @@
+import { LightningElement, api } from 'lwc';
+
+export default class StudentTile extends LightningElement {
+    @api student = {
+        Name: 'Jang HyeMin',
+        PhotoUrl: '/services/images/photo/003B0FakePictId',
+        };
+
+        @api isSelected = false;
+
+        get tileSelected() {
+            return this.isSelected ? "tile selected" : "tile";
+            }
+}
+
