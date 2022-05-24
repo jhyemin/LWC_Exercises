452e14b661abde5d8c1e125b6e9e0314a1bf4134
Jang Hyemin <hmjang@i2max.co.kr>
2022-05-24 13:54:14 +0900

    Exercise 2-9 Contact Selected

--- force-app/Exercises/main/default/lwc/studentTile/studentTile.js
+++ force-app/Exercises/main/default/lwc/studentTile/studentTile.js
@@ -9,11 +9,16 @@ export default class StudentTile extends LightningElement {
         @api isSelected = false;
 
         get tileSelected() {
-            return this.isSelected ? "tile selected" : "tile";
-            }
+            return (this.selectedStudentId===this.student.Id)? "tile selected" : "tile";
+        }
 
     studentClick(){
-        alert(this.student.Name);
+        const evt = new CustomEvent('studentselected', {
+            detail: { studentId: this.student.Id }
+            });
+        this.dispatchEvent(evt) ;
     }
+
+    @api selectedStudentId = '';
 }
 
