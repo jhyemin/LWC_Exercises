122208be59801ee2fff2d5a6a18b381ea9e1c4e6
Jang Hyemin <hmjang@i2max.co.kr>
2022-05-24 10:45:04 +0900

    Exercise 2-7A Completed

--- force-app/Exercises/main/default/lwc/studentBrowser/studentBrowser.js
+++ force-app/Exercises/main/default/lwc/studentBrowser/studentBrowser.js
@@ -1,7 +1,10 @@
-import { LightningElement } from 'lwc';
+import { LightningElement , wire } from 'lwc';
+import getStudents from '@salesforce/apex/StudentBrowser.getStudents';
 
 export default class StudentBrowser extends LightningElement {
-  studentList = [];
+  @wire(getStudents) students;
+  @wire(getStudents, { instructorId: "", courseDeliveryId: ""}) Students;
+
 
     constructor() {
         super();
