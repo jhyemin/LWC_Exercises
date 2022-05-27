8b6acf64fdee0ed8c1c1af5a96786f181fe8fcff
Jang Hyemin <hmjang@i2max.co.kr>
2022-05-27 12:58:38 +0900

    Exercise 5-1 Completed

--- force-app/Exercises/main/default/lwc/studentBrowser/studentBrowser.js
+++ force-app/Exercises/main/default/lwc/studentBrowser/studentBrowser.js
@@ -2,8 +2,9 @@ import { LightningElement , wire } from 'lwc';
 import getStudents from '@salesforce/apex/StudentBrowser.getStudents';
 import { publish, MessageContext } from 'lightning/messageService';
 import SELECTED_STUDENT_CHANNEL from '@salesforce/messageChannel/SelectedStudentChannel__c';
+import { NavigationMixin } from 'lightning/navigation';
 
-export default class StudentBrowser extends LightningElement {
+export default class StudentBrowser extends NavigationMixin(LightningElement) {
   @wire(getStudents) students;
   @wire(getStudents, { instructorId: "", courseDeliveryId: ""}) Students;
     constructor() {
@@ -62,4 +63,18 @@ export default class StudentBrowser extends LightningElement {
       type: "email"
     }
   ];
+
+  handleRowDblClick(event) {
+    const studentId = event.detail.pk;
+    this[NavigationMixin.Navigate]({
+        type: 'standard__recordPage',
+        attributes: {
+        recordId: studentId,
+        objectApiName: 'Contact',
+        actionName: 'edit'
+      }
+    });
+  }
+  
 }
+
