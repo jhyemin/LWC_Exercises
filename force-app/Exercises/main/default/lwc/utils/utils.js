073a7f49bbd5a9e80a0268a62ffa659fb33a41f5
Jang Hyemin <hmjang@i2max.co.kr>
2022-05-27 13:14:25 +0900

    Exercise 5-3 Toast Displays

---
+++ force-app/Exercises/main/default/lwc/utils/utils.js
@@ -0,0 +1,15 @@
+import { LightningElement } from 'lwc';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
+
+export default class Utils extends LightningElement {
+
+    static showToast = (firingComponent, toastTitle, toastBody,
+        variant) => {
+            const evt = new ShowToastEvent({
+                title: toastTitle,
+                message: toastBody,
+                variant: variant
+                });
+            firingComponent.dispatchEvent(evt);
+        }
+}
